b9764a711c7f9f5186665a6bd78bc03651047e95
Carlos Alexandro Becker <caarlos0@gmail.com>
2014-01-29 02:24:34 -0200

    starting planets impl

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -1,11 +1,11 @@
 class MarsRover
   WIND_ROSE = %w(N E S W)
-
   attr_reader :x, :y, :direction
-  def initialize(x, y, direction)
+  def initialize(x, y, direction, planet = InfinitePlanet.new)
     @x = x
     @y = y
     @direction = direction
+    @planet = planet
   end
 
   def command(commands)
@@ -28,6 +28,26 @@ class MarsRover
     increment *= -1 if side.eql?('b')
     value = instance_variable_get(axis)
     instance_variable_set(axis, value + increment)
+  end
+
+  # rotation
+  # move back/forward
+  # if planet given, it should overlap
+  # if list of obstacles given, should stop when obstacle is found
+end
+
+class Planet
+  attr_reader :x, :y
+  def initialize(x, y)
+    @x = x
+    @y = y
+  end
+end
+
+class InfinitePlanet < Planet
+  FIXNUM_MAX = (2**(0.size * 8 -2) -1)
+  def initialize
+    super(FIXNUM_MAX, FIXNUM_MAX)
   end
 end
 
